6290b51039277321f8a9c18ee33606f4254558c5
AlexKapic <alexkapic@gmail.com>
2021-05-07 23:59:07 +0300

    Complited all tasks

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -8,16 +8,32 @@ export function createFighterPreview(fighter, position) {
   });
 
   // todo: show fighter info (image, name, health, etc.)
+  if (fighter) {
+    const fighterName = createElement({ tagName: 'div', className: 'fighterName' });
+    fighterName.innerText = `${fighter.name}`;
+    fighterElement.appendChild(fighterName);
+
+    const fighterInfo = createElement({ tagName: 'div', className: 'fighterInfo' });
+    for (const key in fighter) {
+      if (key !== '_id' && key !== 'source' && key !== 'name') {
+        const infoElement = createElement({ tagName: 'div', className: 'fighterInfo' });
+        infoElement.innerText = `${key.toUpperCase()} : ${fighter[key]}`;
+        fighterInfo.appendChild(infoElement);
+      }
+    }
+    fighterElement.appendChild(createFighterImage(fighter));
+    fighterElement.appendChild(fighterInfo);
+  }
 
   return fighterElement;
 }
 
 export function createFighterImage(fighter) {
   const { source, name } = fighter;
-  const attributes = { 
-    src: source, 
+  const attributes = {
+    src: source,
     title: name,
-    alt: name 
+    alt: name,
   };
   const imgElement = createElement({
     tagName: 'img',
